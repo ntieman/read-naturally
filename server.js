ad8ca4e820d559dac1db1fa1b759f3ac52fe0b8d
Nicholas Tieman <nicholas.tieman@gmail.com>
2022-05-06 22:44:15 -0500

    Stop serving Expo from Express.

--- server.js
+++ server.js
@@ -159,7 +159,11 @@ app.get('/api/students', (_, res) => {
     res.json(rows);
   });
 });
-
+/*
+app.get('mobile/*', (req,res) =>{
+  res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+*/
 app.get('*', (req,res) =>{
   res.sendFile(path.join(__dirname + '/public/index.html'));
 });
